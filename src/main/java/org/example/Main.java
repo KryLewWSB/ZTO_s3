1838dfd43eea73b1d6e9051891421064a3490870
Krystian <krystian.lewandowski@teb.edu.pl>
2023-10-29 13:20:42 +0100

    5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,8 @@ public class Main {
         Pracownik pracownik = new Pracownik("Adam", "Kowalski", 5000);
         Kierownik kierownik = new Kierownik("Adela", "Nowakowska", 8000, 2000);
 
-        System.out.println("Imię pracownik: " + pracownik.imie +
-                            "Nazwisko pacownik: " + pracownik.nazwisko +
-                            "Wypłata pracownik: " + pracownik.wyplata);
-        System.out.println("Imię kierownik: " + kierownik.imie +
-                            "Nazwisko kierownik: " + kierownik.nazwisko +
-                            "Wypłata kierownik: " + kierownik.wyplata +
-                            "Premia kierownicza: "  + kierownik.premiaKierownicza);
-
+        System.out.println(pracownik.getData());
+        System.out.println("Premia kierownicza: "  + kierownik.getData() +
+                            "\nPremia kierownicza: "  + kierownik.premiaKierownicza);
     }
 }
--- src/main/java/org/example/Pracownik.java
+++ src/main/java/org/example/Pracownik.java
@@ -1,13 +1,18 @@
 package org.example;
 
 public class Pracownik {
-    String imie;
-    String nazwisko;
-    int wyplata;
+    private String imie;
+    private String nazwisko;
+    private int wyplata;
     public Pracownik(String imie, String nazwisko, int wyplata){
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.wyplata = wyplata;
+    }
+    public String getData(){
+        return "Imię pracownik: " + this.imie +
+                "\nNazwisko pracownik:" + this.nazwisko +
+                "\nWypłata pracownik:" + this.wyplata;
     }
 
 }
