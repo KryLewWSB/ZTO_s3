dc39277b3330d44e44f6247ac4e1a0d517b5c6e8
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-18 10:50:43 +0100

    7f

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.firma.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,15 +16,55 @@ public class Main {
 
         List<Osoba> listaOsob = new ArrayList<>();
         FabrykaOsob fabrykaOsob = new FabrykaOsob();
-        listaOsob.add(fabrykaOsob.utworzOsobe(Stanowisko.PRACOWNIK));
-        listaOsob.add(fabrykaOsob.utworzOsobe(Stanowisko.KIEROWNIK));
-        listaOsob.add(fabrykaOsob.utworzOsobe(Stanowisko.KIEROWNIK));
 
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            System.out.println("Podaj czy chcesz dodać pracownik, kierownik, x - zakończy dodawanie");
+            String wartosc = scan.next();
+            if(wartosc.equals("x")){
+                break;
+            }else if(wartosc.equals("pracownik")){
+                listaOsob.add(fabrykaOsob.utworzOsobe(Stanowisko.PRACOWNIK));
+                int index = listaOsob.size()-1;
 
-        listaOsob.get(0).setImie("Adam");
-        listaOsob.get(0).setNazwisko("Kowal");
-        listaOsob.get(0).setDzial("Księgowość");
-        listaOsob.get(0).setWynagrodzenie(6000);
+                System.out.println("Podaj imię pracownika: ");
+                String imie = scan.next();
+                listaOsob.get(index).setImie(imie);
+
+                System.out.println("Podaj nazwisko pracownika: ");
+                String nazwisko = scan.next();
+                listaOsob.get(index).setNazwisko(nazwisko);
+
+                System.out.println("Podaj dział pracownika: ");
+                String dzial = scan.next();
+                listaOsob.get(index).setDzial(dzial);
+
+                System.out.println("Podaj wynagrodzenie pracownika: ");
+                int wynagrodzenie = scan.nextInt();
+                listaOsob.get(index).setWynagrodzenie(wynagrodzenie);
+
+            }else if(wartosc.equals("kierownik")){
+                listaOsob.add(fabrykaOsob.utworzOsobe(Stanowisko.KIEROWNIK));
+                int index = listaOsob.size()-1;
+
+                System.out.println("Podaj imię kierownik: ");
+                String imie = scan.next();
+                listaOsob.get(index).setImie(imie);
+
+                System.out.println("Podaj nazwisko kierownik: ");
+                String nazwisko = scan.next();
+                listaOsob.get(index).setNazwisko(nazwisko);
+
+                System.out.println("Podaj dział kierownik: ");
+                String dzial = scan.next();
+                listaOsob.get(index).setDzial(dzial);
+
+                System.out.println("Podaj wynagrodzenie kierownik: ");
+                int wynagrodzenie = scan.nextInt();
+                listaOsob.get(index).setWynagrodzenie(wynagrodzenie);
+            }
+
+        }
 
         for (Osoba osoba : listaOsob) {
             osoba.wypiszDane();
